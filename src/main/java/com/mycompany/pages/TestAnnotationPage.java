494c520c3d0d1365c1a8526dcd39cc757f17526d
brennanmanion <brennanman007@gmail.com>
2021-03-16 20:16:28 -0600

    added salt and password to users

--- src/main/java/com/mycompany/pages/TestAnnotationPage.java
+++ src/main/java/com/mycompany/pages/TestAnnotationPage.java
@@ -7,6 +7,9 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
 
 import com.mycompany.SpringSampleBean;
+import com.mycompany.pojo.User;
+import com.mycompany.services.UserService;
+import com.mycompany.util.SecurityUtil;
 
 @MountPath("testAnnotationPage")
 public class TestAnnotationPage extends WebPage {
@@ -14,13 +17,13 @@ public class TestAnnotationPage extends WebPage {
 
 	@SpringBean
 	private SpringSampleBean springSampleBean;
+	
+	@SpringBean
+	private UserService userService;	
 
 	public TestAnnotationPage(final PageParameters parameters) {
 		super(parameters);
-		add(new Label("title", "Test Annotation Page").setRenderBodyOnly(true));
-		add(new Label("success_note", springSampleBean.getText()));
-
-		// TODO Add your page's components here
+		add(new Label("success_note", "test"));
 
 	}
 }
